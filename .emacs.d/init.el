9c3e519890bd360ce00d45ec4c762e75da494f66
myTerminal <team.terminal@gmail.com>
2019-10-24 22:38:40 -0500

    rename files to remove leading sequence-numbers

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,12 +3,12 @@
   (current-time))
 
 ;;Load configuration files
-(load-file "~/.emacs.d/super-emacs/00-system.el")
-(load-file "~/.emacs.d/super-emacs/01-repositories.el")
-(load-file "~/.emacs.d/super-emacs/02-packages.el")
-(load-file "~/.emacs.d/super-emacs/03-interface.el")
-(load-file "~/.emacs.d/super-emacs/04-misc.el")
-(load-file "~/.emacs.d/super-emacs/05-key-bindings.el")
+(load-file "~/.emacs.d/super-emacs/system.el")
+(load-file "~/.emacs.d/super-emacs/repositories.el")
+(load-file "~/.emacs.d/super-emacs/packages.el")
+(load-file "~/.emacs.d/super-emacs/interface.el")
+(load-file "~/.emacs.d/super-emacs/misc.el")
+(load-file "~/.emacs.d/super-emacs/key-bindings.el")
 
 ;;Print welcome message
 (princ (concat "Welcome to super-emacs!"
