0a8e50c2499c742d5f93c8dd9bed821d30c7b73c
myTerminal <team.terminal@gmail.com>
2019-12-11 21:44:41 -0600

    set hooks for flyspell-mode in 'text-mode'

--- .emacs.d/super-emacs/config-misc.el
+++ .emacs.d/super-emacs/config-misc.el
@@ -40,6 +40,8 @@
       (add-to-list 'exec-path (file-name-directory path-to-aspell))
       (setq ispell-program-name "aspell")
       (setq ispell-extra-args '("--sug-mode=ultra" "--lang=en_US")))))
+(add-hook 'text-mode-hook
+          'flyspell-mode)
 
 ;;Package archives and 'customize'
 (setq custom-file
