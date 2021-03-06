d03feddfdf27f98ca591969a9ca1a579f3e91939
myTerminal <team.terminal@gmail.com>
2021-03-05 23:24:12 -0600

    set up foundation for basic <> standard split

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,8 +1,27 @@
+;; Initialize root
 (defvar se/config-root
   (file-name-directory load-file-name))
-(load (expand-file-name "super-emacs/core"
-			se/config-root))
-(se/load-file "standard/misc")
-(se/load-file "standard/packages")
-(se/load-file "standard/key-bindings")
-(se/print-startup-message)
+
+(defun se/start-basic ()
+  "Loads basic configs"
+  (se/load-file "basic/misc")
+  (se/load-file "basic/packages")
+  (se/load-file "basic/key-bindings"))
+
+(defun se/start-standard ()
+  "Loads standard configs"
+  (se/load-file "standard/misc")
+  (se/load-file "standard/packages")
+  (se/load-file "standard/key-bindings"))
+
+(defun se/start ()
+  "Loads core and then conditionally loads configs"
+  (load (expand-file-name "super-emacs/core"
+			              se/config-root))
+  (se/start-basic)
+  (if (display-graphic-p)
+      (se/start-standard))
+  (se/print-startup-message))
+
+;; Start
+(se/start)
---
+++ .emacs.d/super-emacs/basic/misc-custom.el
@@ -0,0 +1,3 @@
+;; Place your customizations to 'config-misc.el' here.
+;; Future updates to super-emacs will very less likely change this file so that
+;; your customizations can remain free of merge conflicts.
