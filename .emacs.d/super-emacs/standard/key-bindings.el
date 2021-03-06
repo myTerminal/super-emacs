24c177cc74a795c428de01cbe7b35ceee5b30f68
myTerminal <team.terminal@gmail.com>
2021-03-05 23:42:55 -0600

    split: key-bindings.el

--- .emacs.d/super-emacs/standard/key-bindings.el
+++ .emacs.d/super-emacs/standard/key-bindings.el
@@ -1,32 +1,7 @@
-(defvar se/my-keyboard-bindings
-  '(;;Buffer/Window management
-    ("C-x b" . ivy-switch-buffer)
-    ("C-x C-f" . counsel-find-file)
-    ("C-x C-r" . counsel-recentf)
-    ("C-S-<up>" . buf-move-up)
-    ("C-S-<down>" . buf-move-down)
-    ("C-S-<left>" . buf-move-left)
-    ("C-S-<right>" . buf-move-right)
-    ("C-;" . ace-window)
-    ;;Text-editing
-    ("C-}" . mc/mark-next-like-this)
-    ("C-{" . mc/mark-previous-like-this)
-    ("C-|" . mc/mark-all-like-this)
-    ("M-y" . counsel-yank-pop)
-    ("M-/" . undo-tree-visualize)
-    ("C-s" . swiper)
-    ("<f5>" . se/reload-current-file)
-    ;;Navigation
-    ("C->" . ace-jump-mode)
-    ("C-<" . ace-jump-mode-pop-mark)
-    ("C-\," . neotree-toggle)
-    ("C-x p" . projectile-mode)
-    ;;Misc
-    ("C-c M-x" . execute-extended-command)
-    ("M-x" . counsel-M-x)
+(defvar se/my-keyboard-bindings-standard
+  '(;;Misc
     ("C-)" . theme-looper-enable-next-theme)
-    ("C-(" . theme-looper-enable-previous-theme)
-    ("C-M-'" . myterminal-controls-open-controls)))
+    ("C-(" . theme-looper-enable-previous-theme)))
 
 (mapc 'se/assign-function-to-keys
-      se/my-keyboard-bindings)
+      se/my-keyboard-bindings-standard)
