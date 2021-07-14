296f52c7b267f95c82e5e1efd3effd481b1299fd
myTerminal <team.terminal@gmail.com>
2021-07-14 14:34:57 -0500

    reduce private functions

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,27 +1,25 @@
+(require 'cl-lib)
+
 ;; Initialize root
 (defvar se/config-root
   (file-name-directory load-file-name))
 
-(defun se/start-basic ()
-  "Loads basic configs."
-  (se/load-file "basic/misc")
-  (se/load-file "basic/packages")
-  (se/load-file "basic/key-bindings"))
-
-(defun se/start-standard ()
-  "Loads standard configs."
-  (se/load-file "standard/misc")
-  (se/load-file "standard/packages")
-  (se/load-file "standard/key-bindings"))
-
 (defun se/start ()
   "Loads core and then conditionally loads configs."
-  (load (expand-file-name "super-emacs/core"
-			              se/config-root))
-  (se/start-basic)
-  (if (display-graphic-p)
-      (se/start-standard))
-  (se/print-startup-message))
+  (cl-flet* ((start-basic ()
+                          (se/load-file "basic/misc")
+                          (se/load-file "basic/packages")
+                          (se/load-file "basic/key-bindings"))
+             (start-standard ()
+                             (se/load-file "standard/misc")
+                             (se/load-file "standard/packages")
+                             (se/load-file "standard/key-bindings")))
+    (load (expand-file-name "super-emacs/core"
+			                se/config-root))
+    (start-basic)
+    (if (display-graphic-p)
+        (start-standard))
+    (se/print-startup-message)))
 
 ;; Start
 (se/start)
