398908fee4b825d441bb315cdc29d1bb8446f8de
myTerminal <team.terminal@gmail.com>
2023-03-06 13:34:25 -0600

    delete core.el, merge code into init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,12 +1,23 @@
 (require 'cl-lib)
 
+;; Record startup timestamp
+(defvar se2/invokation-time
+  (current-time))
+
 ;; Initialize root
 (defvar se2/config-root
   (file-name-directory load-file-name))
 
+(defun se2/load-config (config-name)
+  "Loads all the config files for the specified config."
+  (mapc 'load (file-expand-wildcards (concat se2/config-root
+                                             "configs/"
+                                             config-name
+                                             "-*.el"))))
+
 (defun se2/start ()
   "Loads core and then conditionally loads configs."
-  (load (expand-file-name "core"
+  (load (expand-file-name "lib"
 			              se2/config-root))
   (se2/load-config "basic")
   (if (display-graphic-p)
