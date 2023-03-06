cc951227a2062c6fc4d4f056d21e270c457da22f
myTerminal <team.terminal@gmail.com>
2023-03-06 13:23:27 -0600

    implement sequential loading of config files

--- .emacs.d/core.el
+++ .emacs.d/core.el
@@ -6,10 +6,13 @@
 (defvar se2/base-path
   (file-name-directory load-file-name))
 
-(defun se2/load-file (file-path)
-  "Loads the specified file relative to the base-path."
-  (load (concat se2/base-path
-                file-path)))
+(defun se2/load-config (config-name)
+  "Loads all the config files for the specified config."
+  (mapc 'load (file-expand-wildcards (concat se2/base-path
+                                             "configs/"
+                                             config-name
+                                             "-*.el"))))
 
 ;; Load lib.el
-(se2/load-file "lib")
+(load (concat se2/base-path
+              "lib"))
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,22 +6,12 @@
 
 (defun se2/start ()
   "Loads core and then conditionally loads configs."
-  (cl-flet* ((start-basic ()
-                          (se2/load-file "configs/basic-misc")
-                          (se2/load-file "configs/basic-packages")
-                          (se2/load-file "configs/basic-key-bindings")
-                          (se2/load-file "configs/basic-custom"))
-             (start-standard ()
-                             (se2/load-file "configs/standard-misc")
-                             (se2/load-file "configs/standard-packages")
-                             (se2/load-file "configs/standard-key-bindings")
-                             (se2/load-file "configs/standard-custom")))
-    (load (expand-file-name "core"
-			                se2/config-root))
-    (start-basic)
-    (if (display-graphic-p)
-        (start-standard))
-    (se2/print-startup-message)))
+  (load (expand-file-name "core"
+			              se2/config-root))
+  (se2/load-config "basic")
+  (if (display-graphic-p)
+      (se2/load-config "standard"))
+  (se2/print-startup-message))
 
 ;; Start
 (se2/start)
