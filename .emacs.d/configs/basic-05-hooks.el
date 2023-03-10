5749742bd317f1aaaf1eec37695e8349a296fd7c
myTerminal <team.terminal@gmail.com>
2023-03-10 16:19:05 -0600

    sync: basic-hooks from myTerminal/.emacs.d

--- .emacs.d/configs/basic-05-hooks.el
+++ .emacs.d/configs/basic-05-hooks.el
@@ -1,2 +1,19 @@
+(add-hook 'after-init-hook
+          'global-company-mode)
+
+(add-hook 'dired-mode-hook
+          'auto-revert-mode)
+
+(add-hook 'prog-mode-hook
+          'display-line-numbers-mode)
+(add-hook 'prog-mode-hook
+          'outer-spaces-mode)
+
 (add-hook 'text-mode-hook
           'flyspell-mode)
+(add-hook 'text-mode-hook
+          'visual-line-mode)
+
+(add-hook 'yas-minor-mode
+          (lambda ()
+            (yas-activate-extra-mode 'fundamental-mode)))
