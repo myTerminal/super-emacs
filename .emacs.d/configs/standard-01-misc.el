c5e3c975e325c6e9d85bb145bb72e97eca190218
myTerminal <team.terminal@gmail.com>
2023-03-10 16:23:52 -0600

    sync: standard-misc from myTerminal/.emacs.d

--- .emacs.d/configs/standard-01-misc.el
+++ .emacs.d/configs/standard-01-misc.el
@@ -1,4 +1,3 @@
-;; Interface
 (setq frame-title-format
       "%b - super-emacs")
 (setq use-dialog-box
@@ -7,9 +6,17 @@
 (scroll-bar-mode 0)
 (setq visible-bell
       t)
-(custom-set-faces
- '(default ((t (:height 120)))))
+(set-face-attribute 'mode-line
+                    nil
+                    :box nil)
 (setq org-pretty-entities t
       org-hide-emphasis-markers t
       org-startup-with-inline-images t
       org-image-actual-width '(640))
+
+(add-to-list 'default-frame-alist
+             `(font . ,se2/variable/font-default))
+
+(set-frame-size (selected-frame)
+                (car se2/variable/frame-dimensions)
+                (cdr se2/variable/frame-dimensions))
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,6 +12,16 @@
 (defvar se2/config-root
   (file-name-directory user-init-file))
 
+;; Define a few variables
+(defvar se2/variable/color-theme
+  'overcast)
+(defvar se2/variable/font-default
+  "-outline-Liberation Mono-normal-normal-normal-mono-20-*-*-*-c-*-iso8859-1")
+(defvar se2/variable/font-family-for-programming
+  "Fira Code")
+(defvar se2/variable/frame-dimensions
+  '(100 . 30))
+
 (defun se2/load-config (config-name)
   "Loads all the config files for the specified config."
   (mapc 'load (file-expand-wildcards (concat se2/se-root
