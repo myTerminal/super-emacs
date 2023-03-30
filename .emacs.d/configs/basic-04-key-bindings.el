78a0d317822cdc8c6d9e07b3ac26b8be313c21b0
myTerminal <team.terminal@gmail.com>
2023-03-30 15:50:16 -0500

    move visual-line mode from hook to a toggle

--- .emacs.d/configs/basic-04-key-bindings.el
+++ .emacs.d/configs/basic-04-key-bindings.el
@@ -4,10 +4,12 @@
   "
 _l_ line-numbers:  %`display-line-numbers-mode
 _SPC_ outer-spaces:  %`outer-spaces-mode
+_w_ word-wrap:  %`visual-line-mode
 
 "
   ("l" display-line-numbers-mode nil)
   ("SPC" outer-spaces-mode nil)
+  ("w" visual-line-mode nil)
   ("q" nil "Cancel"))
 
 (defhydra se2/hydra-tools (:color blue)
--- .emacs.d/configs/basic-05-hooks.el
+++ .emacs.d/configs/basic-05-hooks.el
@@ -15,8 +15,6 @@
           'electric-pair-local-mode)
 (add-hook 'text-mode-hook
           'flyspell-mode)
-(add-hook 'text-mode-hook
-          'visual-line-mode)
 
 (add-hook 'yas-minor-mode
           (lambda ()
